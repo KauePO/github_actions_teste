2977fb4c39a5a4cc4b08614eed58c58c998baf05
KauePO <kaueperes2004@gmail.com>
2025-09-20 12:05:21 -0300

    test
    cria teste para função de multiplicar da calculadora

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -14,3 +14,8 @@ def test_dividir():
     assert dividir(10, 2) == 5
     with pytest.raises(ValueError):
         dividir(10, 0)
+
+def test_multiplicar():
+    assert multiplicar(10, 2) == 20
+    assert multiplicar(5, 0) == 0
+    assert multiplicar(5, -1) == -5
